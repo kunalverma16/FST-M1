680bd630390ab0cf15a323ea9c5a3b8207d748d0
Kunal verma <kunalverma262@gmail.com>
2025-02-20 11:06:14 +0530

    Add files via upload

---
+++ Python/Activities/Activity7.py
@@ -0,0 +1,6 @@
+numbers=[1,2,3,5]
+sum=0	
+for number in numbers:
+  sum += int(number)
+ 
+print(sum)
---
+++ Python/Activities/Activity8.py
@@ -0,0 +1,14 @@
+# Given list of numbers
+List = [10, 20, 30, 40, 10]
+print("Given list is ", List)
+
+# Get first element in list
+firstElement = List[0]
+# Get last element in list
+lastElement = List[-1]
+
+# Check if first and last element are equal
+if (firstElement == lastElement):
+    print(True)
+else:
+    print(False)
